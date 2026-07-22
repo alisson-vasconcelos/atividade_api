15c4468ef761e31ce01cecb0c785d34ef946022a
alisson-vasconcelos <alisson.f.vasconcelos@gmail.com>
2026-07-22 18:44:59 -0300

    Implement Usuario model, repository, and controller with CRUD operations

--- obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
+++ obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f72a6d9c6fbfb1495b088bff2105193d03e4f4c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c14bffa53c5def0711b33e8f5f6a5ca5211d23ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
